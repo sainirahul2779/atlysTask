9f73bac2b68add1d52a3df18efc9895bcf325269
rahul-ios-779 <108657871+rahul-ios-779@users.noreply.github.com>
2025-10-30 19:58:25 +0530

    Initial Commit

---
+++ Atlys/AtlysApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtlysApp.swift
+//  Atlys
+//
+//  Created by Rahul on 30/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AtlysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Atlys/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Atlys
+//
+//  Created by Rahul on 30/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
